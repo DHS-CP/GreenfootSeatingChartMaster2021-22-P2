376756acafa0a29e1011cc8baf0d146cd0528241
pltw student <pltwtest@PLTW.local>
2021-09-17 09:08:06 -0700

    Changed KilgoreTrout files to VedantAgrawal files

--- VedantAgrawal.java
+++ VedantAgrawal.java
@@ -1,17 +1,17 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * The KilgoreTrout class can be used as a model for your own class that represents you and your seating location in AP CSA
+ * The VedantAgrawal class can be used as a model for your own class that represents you and your seating location in AP CSA
  * 
  * @author Mr. Kaehms
  * @version 2.0 Aug 13, 2019
  * @version 3.0 July 21, 2020
  */
-public class KilgoreTrout extends Student implements SpecialInterestOrHobby
+public class VedantAgrawal extends Student
 {
 
     /**
-     * Constructor for the KilgoreTrout class.
+     * Constructor for the VedantAgrawal class.
      * Constructors are special methods with the same exact name as the class name.  
      * Constructors to not have return types.
      * Constructors can be overloaded. This means we can call a constructor with different sets of parameter
@@ -22,7 +22,7 @@ public class KilgoreTrout extends Student implements SpecialInterestOrHobby
      * @param int s (seat number within row seating arrangement)
      * 
      */
-    public KilgoreTrout(String f, String l, int r, int s) {
+    public VedantAgrawal(String f, String l, int r, int s) {
         firstName=f;
         lastName=l;
         mySeatX=r;
@@ -38,10 +38,10 @@ public class KilgoreTrout extends Student implements SpecialInterestOrHobby
      * Pay attention to how the row and seat variables set the location of the image.  1,1 is the first cell in the upper left
      * of the classroom.
      */
-    public KilgoreTrout() {
-        firstName="Kilgore";
-        lastName="Trout";
-        mySeatX=1;
+    public VedantAgrawal() {
+        firstName="Vedant";
+        lastName="Agrawal";
+        mySeatX=8;
         mySeatY=1;
        // imgFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
        portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
@@ -52,7 +52,7 @@ public class KilgoreTrout extends Student implements SpecialInterestOrHobby
     }
     
      /**
-     * Act - do whatever the KilgoreTrout actor wants to do. This method is called whenever
+     * Act - do whatever the VedantAgrawal actor wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */   
     public void act() 
@@ -66,7 +66,7 @@ public class KilgoreTrout extends Student implements SpecialInterestOrHobby
                 getName();
                 sayName(soundFile);
             
-                myHobby("I like to time travel!");
+                myHobby();
             // Create a "special method for your class and put the call here.  You can twirl your image, resize it, move it around, change transparancy, or a 
             // combination of all of those types of actions, or more. Make sure to save the original image if you manipulate it, so that you can put it back.
             // Call the sitDown() method to move back  to your seat
@@ -93,31 +93,51 @@ public class KilgoreTrout extends Student implements SpecialInterestOrHobby
 
    
     /**
-     * This is a local method specific to the KilgoreTrout class used to animate the character once the image is clicked on.
+     * This is a local method specific to the VedantAgrawal class used to animate the character once the image is clicked on.
      * You should write your own methods to perform your own animation for your character/avatar.
      */
     public void circleClass(){
         setLocation(0,0);
          Greenfoot.delay(10);
         // move right
-        for (int i=1;i<=9;i++){
+        for (int i=1;i<=5;i++){
             setLocation(i,0);
-            Greenfoot.delay(10);
+            Greenfoot.delay(2);
         }
         // move back
         for (int i=1;i<=5;i++){
             setLocation(9,i);
-            Greenfoot.delay(10);
+            Greenfoot.delay(2);
         }      
-         // move left
-        for (int i=9;i>=0;i--){
-            setLocation(i,5);
-            Greenfoot.delay(10);
-        }      
-              // move Forward
+        // move right
+        for (int i=1;i<=5;i++){
+            setLocation(i,0);
+            Greenfoot.delay(2);
+        }
+        // move Forward
         for (int i=5;i>=0;i--){
             setLocation(0,i);
-            Greenfoot.delay(10);
+            Greenfoot.delay(2);
+        }   
+        // move left
+        for (int i=5;i>=0;i--){
+            setLocation(i,5);
+            Greenfoot.delay(2);
+        } 
+        // move back
+        for (int i=1;i<=5;i++){
+            setLocation(9,i);
+            Greenfoot.delay(2);
+        }      
+        // move left
+        for (int i=5;i>=0;i--){
+            setLocation(i,5);
+            Greenfoot.delay(2);
+        } 
+        // move Forward
+        for (int i=5;i>=0;i--){
+            setLocation(0,i);
+            Greenfoot.delay(2);
         }   
            Greenfoot.delay(20);
            returnToSeat();
@@ -127,8 +147,15 @@ public class KilgoreTrout extends Student implements SpecialInterestOrHobby
      * An interface is just a contract for the methods that you will implement in your code.  The College Board no longer
      * tests on abstract classes and interfaces, but it is good to know about them
      */
-     public void myHobby(String s) {
-         System.out.println(s);
+     public void myHobby() {
+         System.out.println("I like to read!");
+         String input = Greenfoot.ask("Do you like to read? (y/n)");
+         if (input == "y") {
+            System.out.println("Me too!");
+         };
+         if (input == "n") {
+            System.out.println("");
+         };
 }
 
 }
