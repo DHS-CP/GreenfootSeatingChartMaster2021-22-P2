93e5a22de15e86477b83d079232a4ada11fdeea2
JaspalK <jessiekhanuja@gmail.com>
2021-09-17 09:02:34 -0700

    Jaspal Khanuja Code(w/ AnishKumar, and Anya's Code))
    This branch contains 3 people's code from the  table group #3, excluding Elizabeth Koh.

--- Classroom.java
+++ Classroom.java
@@ -29,7 +29,7 @@ public class Classroom extends World
     {    
         // Create a new world with 10x6 cells with a cell size of 130x130 pixels.
         super(14, 14, 60); 
-
+        
         prepare();
     }
 
@@ -142,17 +142,26 @@ public class Classroom extends World
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
-    
+
     private void prepare()
     {
         createDeskLayout();
- // Each student needs to create their specific instance following the KilgoreTrout example.
- // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
- // and then clicking on the inspect text
+        // Each student needs to create their specific instance following the KilgoreTrout example.
+        // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
+        // and then clicking on the inspect text
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
-  
+        JaspalKhanuja jaspakhanuja = new JaspalKhanuja();
+        addObject(jaspakhanuja,8,6);
+        jaspakhanuja.assignSeat();
+        
+        AnyaSengupta anyaSengupta = new AnyaSengupta();
+        addObject(anyaSengupta,8,7);
+        AnishkumarSenthil anishkumarSenthil = new AnishkumarSenthil();
+        addObject(anishkumarSenthil,9,7);
+        
+     
     }
     
     public List<Student> getAllStudents(){
@@ -196,32 +205,32 @@ public class Classroom extends World
     
 
    public  void appendFile(String fname, String s){
-   {	
+   {    
       try{
-    	 
+         
         //Specify the file name and path here
-    	File file =new File(fname);
+        File file =new File(fname);
 
-    	/* This logic is to create the file if the
-    	 * file is not already present
-    	 */
-    	if(!file.exists()){
-    	   file.createNewFile();
-    	}
+        /* This logic is to create the file if the
+         * file is not already present
+         */
+        if(!file.exists()){
+           file.createNewFile();
+        }
 
-    	//Here true is to append the content to file
-    	FileWriter fw = new FileWriter(file,true);
-    	//BufferedWriter writer give better performance
-    	BufferedWriter bw = new BufferedWriter(fw);
-    	bw.write(s);
-    	//Closing BufferedWriter Stream
-    	bw.close();
+        //Here true is to append the content to file
+        FileWriter fw = new FileWriter(file,true);
+        //BufferedWriter writer give better performance
+        BufferedWriter bw = new BufferedWriter(fw);
+        bw.write(s);
+        //Closing BufferedWriter Stream
+        bw.close();
 
-	System.out.println("Data successfully appended at the end of file");
+    System.out.println("Data successfully appended at the end of file");
 
       }catch(IOException ioe){
          System.out.println("Exception occurred:");
-    	 ioe.printStackTrace();
+         ioe.printStackTrace();
        }
    }
 }
