a063c1102f93af9da07a6941e7cee3d46c965592
Yougi Jain <62231373+M-Dezy@users.noreply.github.com>
2021-09-18 17:45:37 -0700

    Changed yougijain class animations

--- Classroom.java
+++ Classroom.java
@@ -142,17 +142,20 @@ public class Classroom extends World
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
-    
+
     private void prepare()
     {
         createDeskLayout();
- // Each student needs to create their specific instance following the KilgoreTrout example.
- // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
- // and then clicking on the inspect text
+        // Each student needs to create their specific instance following the KilgoreTrout example.
+        // Your current seatX and seatY can be found by right clicking on the corresponding seat in the Classrom.
+        // and then clicking on the inspect text
         KilgoreTrout kilgoretrout = new KilgoreTrout();
         addObject(kilgoretrout,2,3);
         kilgoretrout.assignSeat();
-  
+
+        YougiJain yougijain = new YougiJain();
+        addObject(yougijain,5,9);
+        //yougijain.setLocation(5,9);
     }
     
     public List<Student> getAllStudents(){
