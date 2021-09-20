c1eaba1688e8891a5de1392c46f9b27e6ffe4e66
Shashank Thapa <mamtakhanka@shashank-air.lan>
2021-09-20 01:01:59 -0700

    initial animations added + images/sounds

---
+++ ShashankThapa.java
@@ -0,0 +1,95 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+/**
+ * Write a description of class ShashankThapa here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ShashankThapa extends Student implements SpecialInterestOrHobby
+{
+    public ShashankThapa(String f, String l, int r, int s) {
+        firstName=f;
+        lastName=l;
+        mySeatX=r;
+        mySeatY=s;
+        portraitFile=f.toLowerCase()+l.toLowerCase()+".jpg";    // Make sure to name your image files firstlast.jpg, all lowercase!!!
+        standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
+        soundFile=f.toLowerCase()+l.toLowerCase()+".wav";  // Make sure to name your sound files firstlast.wav, all lowercase!!!
+        setImage(portraitFile);
+        sitting=true;
+    }
+    public ShashankThapa() {
+        firstName="Shashank";
+        lastName="Thapa";
+        mySeatX=10;
+        mySeatY=10;
+       // imgFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
+       portraitFile=firstName.toLowerCase()+ lastName.toLowerCase()+".jpg";
+       standingFile=firstName.toLowerCase()+ lastName.toLowerCase()+"-standing.jpg";
+        soundFile=firstName.toLowerCase()+ lastName.toLowerCase()+".wav"; 
+        setImage(portraitFile);
+        sitting=true;
+    }
+    /**
+     * Act - do whatever the ShashankThapa wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        if(Greenfoot.mouseClicked(this)){
+          //  if (sitting){
+                sitting=false;
+                setImage("shashankthapa.jpg");
+                System.out.println(""); // Print a blank line to create space between any student output.
+                 sayName("shashankthapa.wav");
+                getName();
+                
+            
+                myHobby("I like to play video games and sleep");
+            // Create a "special method for your class and put the call here.  You can twirl your image, resize it, move it around, change transparancy, or a 
+            // combination of all of those types of actions, or more. Make sure to save the original image if you manipulate it, so that you can put it back.
+            // Call the sitDown() method to move back  to your seat
+            
+                circleClass();  // Kilgore Trount's special method... Please write one of your own. You can use this, but please modify it and be creative.
+           
+                sitDown();
+    }
+}   
+    public void getName(){
+        System.out.println("My name is " + firstName + " " + lastName);
+    }
+    public void circleClass(){
+        setLocation(8, 10);
+        Greenfoot.delay(20);
+        
+        // move right
+        for (int i=1;i<=13;i++){
+            setLocation(i,10);
+            Greenfoot.delay(5);
+            turn(90);
+        }
+         for (int i=10;i>=0;i--){
+
+             setLocation(12,i);
+            Greenfoot.delay(5);
+            turn(90);
+        } 
+        for (int i=12;i>=0;i--){
+            setLocation(i,0);
+            Greenfoot.delay(5);
+            turn(90);
+        }      
+         for (int i=1;i<=10;i++){
+            setLocation(1,i);
+            Greenfoot.delay(5);
+            turn(90);
+        }  
+           Greenfoot.delay(1);
+           returnToSeat();
+           turn(90);
+    }
+     public void myHobby(String s) {
+         System.out.println(s);
+}
+
+}
